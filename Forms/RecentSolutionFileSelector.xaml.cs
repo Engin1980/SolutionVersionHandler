8425f79622fca27721169ef6aad08c9ef424828f
Marek Vajgl <engin@seznam.cz>
2025-11-09 23:36:45 +0100

    Add button to delete recent solution files

--- Forms/RecentSolutionFileSelector.xaml.cs
+++ Forms/RecentSolutionFileSelector.xaml.cs
@@ -52,5 +52,42 @@ namespace SolutionVersionHandler.Forms
     {
       this.DialogResult = false;
     }
+
+    private void btnDeleteRecentSolutionFile_Click(object sender, RoutedEventArgs e)
+    {
+      // Delete the selected recent solution file from the list
+      // before deletion, ask for confirmation
+      // The delete button is inside the ComboBox item template; determine the item from the button's DataContext
+      if (sender is not Button btn)
+        return;
+
+      if (btn.DataContext is not AppViewModel.RecentSolutionFile selected)
+        return;
+
+      var message = $"Do you really want to remove the recent solution entry:\n\n{selected.FileName}?";
+      var caption = "Confirm delete";
+      var result = MessageBox.Show(this, message, caption, MessageBoxButton.YesNo, MessageBoxImage.Question);
+      if (result != MessageBoxResult.Yes)
+        return;
+
+      var recentList = AppViewModel.Instance?.RecentSolutionFiles;
+      if (recentList == null)
+        return;
+
+      // Remove the selected item
+      recentList.Remove(selected);
+
+      // If the removed item was currently selected in the ComboBox, clear ComboBox selection
+      if (Equals(cmbRecentSolutions.SelectedItem, selected))
+      {
+        cmbRecentSolutions.SelectedItem = null;
+      }
+
+      // If the removed item was currently selected in this dialog, clear SelectedSolutionFile
+      if (SelectedSolutionFile == selected)
+      {
+        SelectedSolutionFile = null;
+      }
+    }
   }
 }
