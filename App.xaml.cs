5533457fce94d41b6c96194966286f505eadd612
Marek Vajgl <engin@seznam.cz>
2025-11-09 22:19:10 +0100

    Add load/store recent solution files.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,55 @@
-﻿using System.Configuration;
+﻿using SolutionVersionHandler.Model;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
 namespace SolutionVersionHandler
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    public static string RecentSolutionFilesFile => System.IO.Path.Combine(
+      Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+      "SolutionVersionHandler",
+      "recent_solutions.txt");
+    protected override void OnStartup(StartupEventArgs e)
     {
+      base.OnStartup(e);
+      LoadRecentSolutionFiles();
     }
+
+    private void LoadRecentSolutionFiles()
+    {
+      if (System.IO.File.Exists(RecentSolutionFilesFile))
+      {
+        var lines = System.IO.File.ReadAllLines(RecentSolutionFilesFile);
+        lines
+          .Select(q => new AppViewModel.RecentSolutionFile(q))
+          .ToList()
+          .ForEach(q => AppViewModel.Instance.RecentSolutionFiles.Add(q));
+      }
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+      SaveRecentSolutionFiles();
+      base.OnExit(e);
+    }
+
+    private void SaveRecentSolutionFiles()
+    {
+      string dir = System.IO.Path.GetDirectoryName(RecentSolutionFilesFile)!;
+      if (System.IO.Directory.Exists(dir) == false)
+      {
+        System.IO.Directory.CreateDirectory(dir);
+      }
+      var lines = AppViewModel.Instance.RecentSolutionFiles
+        .Select(q => q.FilePath)
+        .ToArray();
+      System.IO.File.WriteAllLines(RecentSolutionFilesFile, lines);
+    }
+  }
 
 }
